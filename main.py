e0e62c8e93e7538964239eb19e3d0d5ef56c720f
BraisRS <brt>
2020-11-02 19:10:24 +0100

    Version 0.1.1b: -Añadida ventana emergente de confirmacion de salida(Solo interfaz) -Añadido codigo de ventana(No es estable)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Ventana import *
+from vensalir import *
 import sys, var , events , clients
 
 class Main(QtWidgets.QMainWindow):
@@ -15,6 +16,8 @@ class Main(QtWidgets.QMainWindow):
 
         var.rbtsex = (var.ui.rbtFem, var.ui.rbtMasc)
 
+        QtWidgets.QAction(self).triggered.connect(self.close)
+
         for i in var.rbtsex:
             i.toggled.connect(events.Eventos.selSexo)
 
@@ -25,6 +28,15 @@ class Main(QtWidgets.QMainWindow):
 
         clients.Clientes.cargarProv()
         var.ui.cmbProv.activated[str].connect(clients.Clientes.selProv)
+
+
+class DialogSalir(QtWidgets.QDialog):
+    def __init__(self):
+        super(DialogSalir,self).__init__()
+        var.dialog = Ui_venSalir()
+        var.dialog.setupUi(self)
+        var.dialog.btnBox.button(QtWidgets.QDialogButtonBox.Yes).clicked.connect(events.Eventos.Salir())
+        var.dialog.btnBox.button(QtWidgets.QDialogButtonBox.No).clicked.connect(events.Eventos.Salir())
 
 
 if __name__ == '__main__':
